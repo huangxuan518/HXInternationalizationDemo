36a122ab7b8d10f56c6fc694e4988d65024479b0
huangxuan <770493410@qq.com>
2016-07-28 14:18:38 +0800

    Initial Commit

---
+++ MyInternational/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyInternational
+//
+//  Created by 黄轩 on 16/7/28.
+//  Copyright © 2016年 黄轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
